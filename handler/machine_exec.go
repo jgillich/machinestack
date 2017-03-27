a643508b39685822b1fd00f00576dbbfe3cc5392
Jakob Gillich <jakob@gillich.me>
2017-03-27 17:27:04 +0200

    implement ExecControl

--- handler/machine_exec.go
+++ handler/machine_exec.go
@@ -95,6 +95,26 @@ func (h *Handler) ExecIO(c echo.Context) error {
 }
 
 func (h *Handler) ExecControl(c echo.Context) error {
-	// TODO
-	return nil
+	id := c.Param("id")
+	exec, ok := execs[id]
+	if !ok {
+		return c.String(http.StatusNotFound, fmt.Sprintf("exec '%s' not found", id))
+	}
+
+	upgrader := websocket.Upgrader{}
+
+	conn, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return nil
+	}
+	defer conn.Close()
+
+	for {
+		var msg driver.ControlMessage
+		if err := conn.ReadJSON(msg); err != nil {
+			return err
+		}
+
+		exec.control <- msg
+	}
 }
