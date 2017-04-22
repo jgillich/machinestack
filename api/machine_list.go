eb81f840751caadeff986a78a20048918d96366c
Jakob Gillich <jakob@gillich.me>
2017-04-22 19:03:07 +0200

    jsonapi expects list of pointers

--- api/machine_list.go
+++ api/machine_list.go
@@ -13,7 +13,7 @@ import (
 func (h *Handler) MachineList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	claims := r.Context().Value("user").(jwt.Token).Claims.(jwt.MapClaims)
 
-	var machines []model.Machine
+	var machines []*model.Machine
 	if err := h.DB.Model(&machines).Where("user_id = ?", claims["id"]).Select(); err != nil {
 		WriteInternalError(w, "session info: db error", err)
 		return
