a6d57786893b918b35667fd87bcdf30543b527ac
Jakob Gillich <jakob@gillich.me>
2017-04-25 20:34:14 +0200

    make test token quota match actual type

--- api/api_test.go
+++ api/api_test.go
@@ -16,7 +16,7 @@ import (
 var (
 	testToken = &jwt.Token{Claims: jwt.MapClaims{
 		"id":            1,
-		"machine_quota": map[string]int{"instances": 10, "cpu": 1, "ram": 1},
+		"machine_quota": map[string]interface{}{"instances": float64(10), "cpu": float64(1), "ram": float64(1)},
 	}}
 	testScheduler, _ = scheduler.NewMockScheduler(nil)
 	mockScheduler    = testScheduler.(*scheduler.MockScheduler)
